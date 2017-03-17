858d5a700b485cbcc65e278094d316ed9b0b0cf3
j-west <westja89@gmail.com>
2017-03-16 22:10:54 -0500

    Added test for addProductToSession.  Added a function to clear the sessionInfo object

--- test/session-info-module-test.js
+++ test/session-info-module-test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {assert} = require('chai')
-const {getSessionInfo, setSessionInfo} = require('../lib/sessionInfoModule.js')
+const {getSessionInfo, setSessionInfo, addProductToSession, clearSession} = require('../lib/sessionInfoModule.js')
 
 describe(`getSessionInfo`, function() {
 	it(`should return an object`, function() {
@@ -11,5 +11,13 @@ describe(`getSessionInfo`, function() {
 describe(`setSessionInfo`, function() {
 	it(`should be a function that changes a property of the object`, function() {
 		setSessionInfo()
+	})
+})
+describe(`addProductToSession`, function() {
+	it(`should be a function that changes a property of the object`, function() {
+		clearSession();
+		setSessionInfo("shoppingCart", [])
+		addProductToSession({"James": "student"})
+		assert.deepProperty(getSessionInfo().shoppingCart[0], "James")
 	})
 })
